da74dff569934dd0fc3cfc1b22ee961d22bee182
dimasiqball <dimasiqball087@gmail.com>
2023-06-05 14:11:14 +0700

    testing push

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,27 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import compression from 'compression';
 import { ConfigService } from '@nestjs/config';
-import { CommonInterface } from './common/config/config-interface'
+import { CommonInterface } from './common/config/config-interface';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: '*',
     credentials: false,
-  })
-  app.use(helmet({
-    hidePoweredBy: true,
-  }));
-  app.use(compression())
+  });
+  app.use(compression());
+  app.enable('trust proxy');
+  app.use(
+    helmet({
+      hidePoweredBy: true,
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.setGlobalPrefix('api');
   const config = app.get(ConfigService);
-  const logger = new Logger;
-  await app.listen(config.get<CommonInterface>('common').port)
-  logger.debug('server running on ' + config.get<CommonInterface>('common').appUrl)
+  const logger = new Logger();
+  await app.listen(config.get<CommonInterface>('common').port);
+  logger.debug(
+    'server running on ' + config.get<CommonInterface>('common').appUrl,
+  );
 }
 bootstrap();
